74cb51ea87e63bf6c7f274a3c063fa093ac78b35
min-k2 <ggomrang2@gmail.com>
2025-03-17 11:08:19 +0900

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,50 @@
+$(function () {
+
+  $(window).scroll(function () {
+    let pos = $(this).scrollTop();
+    if (pos > 300) {
+      $("#header").addClass("fix");
+    } else {
+      $("#header").removeClass("fix");
+    }
+
+    /*     if (pos > 500) {
+          $("body").css({ "background": "#eee" });
+        } else {
+          $("body").css({ "background": "#fff" });
+        } */
+  });
+
+  // depth2
+  $(".depth2 , .depth2_bg").hide();
+  $(".gnb > li").mouseenter(function () {
+    $(".search").fadeOut();
+    $(".btn_search").removeClass("active");
+    $("#header").addClass("active");
+    $(".depth2_bg").stop().fadeIn();
+    $(this).find(".depth2").stop().fadeIn();
+  });
+  $(".gnb > li").mouseleave(function () {
+    $("#header").removeClass("active");
+    $(".depth2_bg").stop().fadeOut();
+    $(this).find(".depth2").stop().fadeOut();
+  });
+
+
+  // search
+  $(".search").hide();
+  $(".btn_search").click(function () {
+    $(this).toggleClass("active");
+    $(".search").fadeToggle();
+    $("#header").toggleClass("active");
+  });
+
+  // mgnb
+  $(".mgnb_wrap").hide();
+  $(".btn_ham").click(function () {
+    $(".mgnb_wrap").fadeIn();
+  });
+  $(".mgnb_close").click(function () {
+    $(".mgnb_wrap").fadeOut();
+  });
+});
